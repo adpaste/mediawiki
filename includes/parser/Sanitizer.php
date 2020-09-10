0e13b44a43cb2e1f3d62858b2d3f3cdc54aac476
grunny <mwgrunny@gmail.com>
2020-09-10 23:52:20 +1000

    (GPUCP-352) Remove var from the CSS sanitizer on Gamepedia wikis

--- includes/parser/Sanitizer.php
+++ includes/parser/Sanitizer.php
@@ -631,13 +631,15 @@ class Sanitizer {
 	 * @return string
 	 */
 	public static function checkCss( $value ) {
+		global $wgEnableHydraFeatures;
 		$value = self::normalizeCss( $value );
 
 		// Reject problematic keywords and control characters
 		if ( preg_match( '/[\000-\010\013\016-\037\177]/', $value ) ||
 			strpos( $value, UtfNormal\Constants::UTF8_REPLACEMENT ) !== false ) {
 			return '/* invalid control char */';
-		} elseif ( preg_match(
+		/** Fandom change begin -- we need to remove "| var\s*\(" on GP wikis **/
+		} elseif ( empty( $wgEnableHydraFeatures ) && preg_match(
 			'! expression
 				| filter\s*:
 				| accelerator\s*:
@@ -651,7 +653,21 @@ class Sanitizer {
 				| var\s*\(
 			!ix', $value ) ) {
 			return '/* insecure input */';
+		} elseif ( !empty( $wgEnableHydraFeatures ) && preg_match(
+			'! expression
+				| filter\s*:
+				| accelerator\s*:
+				| -o-link\s*:
+				| -o-link-source\s*:
+				| -o-replace\s*:
+				| url\s*\(
+				| image\s*\(
+				| image-set\s*\(
+				| attr\s*\([^)]+[\s,]+url
+			!ix', $value ) ) {
+			return '/* insecure input */';
 		}
+		/** Fandom change end **/
 		return $value;
 	}
 
